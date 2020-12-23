3cf1ea544c78a005e2395256d5451822d5047503
Ian <ian.manalo1026@gmail.com>
2020-12-23 23:27:43 +0800

    new template

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "appOne/static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "appOne/static")]
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
