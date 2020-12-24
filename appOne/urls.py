a7611a142954a4a3adf565d5b60e49279709e7c9
Ian <ian.manalo1026@gmail.com>
2020-12-24 15:53:39 +0800

    test for deployment

--- appOne/urls.py
+++ appOne/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -10,3 +11,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
+urlpatterns += staticfiles_urlpatterns()
