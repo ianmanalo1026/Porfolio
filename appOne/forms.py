c1b7643f5b18137977ee4b496f0e26a05a5f63d8
Ian <ian.manalo1026@gmail.com>
2021-03-23 22:04:11 +0800

    update

--- appOne/forms.py
+++ appOne/forms.py
@@ -1,19 +1,18 @@
 from django import forms
-from .models import Content, Images
+from django.db.models import fields
 
-class PostForm(forms.ModelForm):
-    title = forms.CharField(max_length=128)
-    content = forms.CharField(max_length=245)
-    website = forms.URLField(max_length=245)
-    github = forms.URLField(max_length=245)
- 
+from .models import Content
+
+
+
+class ContentForm(forms.ModelForm):
+
     class Meta:
         model = Content
-        fields = ('title', 'content', 'website', 'github')
- 
- 
-class ImageForm(forms.ModelForm):
-    image = forms.ImageField(label='Image')    
-    class Meta:
-        model = Images
-        fields = ('image', )
+        fields = [
+            'title',
+            'content',
+            'website',
+            'github',
+            'image',
+        ]
