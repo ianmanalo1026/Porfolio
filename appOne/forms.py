2eb79aeaa6f5c0dd699014991175c859847d641e
Ian <ian.manalo1026@gmail.com>
2021-03-24 17:05:46 +0800

    update

--- appOne/forms.py
+++ appOne/forms.py
@@ -7,6 +7,12 @@ from .models import Content
 
 class ContentForm(forms.ModelForm):
     content = forms.CharField(widget=CKEditorWidget())
+    title = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
+    website = forms.URLField(max_length=50, required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
+    github = forms.URLField(max_length=50, required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
     class Meta:
         model = Content
         fields = '__all__'
+        
+    def clean(self):
+        cleaned_data = super().clean()
